bca9a4e26ef173b84a5ea10f7eb886625a99f3de
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2014-03-23 16:13:45 +0100

    #9: mer strømlinjet kode i BlogBuilderTest

--- hjemme/hjemme-module/src/test/java/nu/hjemme/module/domain/BlogBuilderTest.java
+++ hjemme/hjemme-module/src/test/java/nu/hjemme/module/domain/BlogBuilderTest.java
@@ -1,42 +1,46 @@
 package nu.hjemme.module.domain;
 
 import nu.hjemme.module.persistence.UserEntity;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 /** @author Tor Egil Jacobsen */
 public class BlogBuilderTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
-    public void willValidateEntityWhenBuilding() throws Exception {
-        try {
-            BlogBuilder.init().build();
-            fail("The Blog should not be valid without a title");
-        } catch (IllegalArgumentException iae) {
-            assertThat("Validation message", iae.getMessage(),
-                    is(equalTo(BlogBuilder.THE_BLOG_MUST_HAVE_A_TITLE)));
-        }
+    public void skalIkkeByggeUtenTittel() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(BlogBuilder.THE_BLOG_MUST_HAVE_A_TITLE);
 
-        try {
-            BlogBuilder.init().appendTitle("").build();
-            fail("The Blog should not be valid when the title is empty");
-        } catch (IllegalArgumentException iae) {
-            assertThat("Validation message", iae.getMessage(),
-                    is(equalTo(BlogBuilder.THE_BLOG_MUST_HAVE_A_TITLE)));
-        }
+        BlogBuilder.init().appendUser(new UserEntity()).build();
+    }
 
-        try {
-            BlogBuilder.init().appendTitle("title").build();
-            fail("The Blog should not be valid if it does not belong to a user");
-        } catch (IllegalArgumentException iae) {
-            assertThat("Validation message", iae.getMessage(),
-                    is(equalTo(BlogBuilder.THE_BLOG_MUST_BELONG_TO_A_USER)));
-        }
+    @Test
+    public void skalIkkeByggeMedTomTittel() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(BlogBuilder.THE_BLOG_MUST_HAVE_A_TITLE);
 
+        BlogBuilder.init().appendTitle("").appendUser(new UserEntity()).build();
+    }
+
+    @Test
+    public void skalIkkeByggeUtenBruker() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(BlogBuilder.THE_BLOG_MUST_BELONG_TO_A_USER);
+
+        BlogBuilder.init().appendTitle("title").build();
+    }
+
+    @Test
+    public void skalByggeMedTittelOgBruker() {
         Blog guestBookEntryEntity = BlogBuilder.init().appendTitle("title").appendUser(new UserEntity()).build();
 
         assertThat("BlogEntity", guestBookEntryEntity, is(notNullValue()));
