26f7491952d7dd440ec1f841e9f3aaca5dd7280e
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2014-05-04 15:16:08 +0200

    #17: fjernet UserFacade fra spring context xml,

--- hjemme/hjemme-business/src/main/java/nu/hjemme/business/service/UserFacadeImpl.java
+++ hjemme/hjemme-business/src/main/java/nu/hjemme/business/service/UserFacadeImpl.java
@@ -4,31 +4,22 @@ import nu.hjemme.client.datatype.UserName;
 import nu.hjemme.client.domain.User;
 import nu.hjemme.client.service.UserFacade;
 
-import java.util.HashMap;
 import java.util.Map;
-
-import static nu.hjemme.business.dao.UserDao.initJactor;
-import static nu.hjemme.business.dao.UserDao.initTip;
 
 /** @author Tor Egil Jacobsen */
 public class UserFacadeImpl implements UserFacade {
-    private static final Map<UserName, User> STANDARD_USERS = new HashMap<>(2);
+    private final Map<UserName, User> defaultUsers;
 
-    public UserFacadeImpl() {
-        initStandardUsers();
+    public UserFacadeImpl(Map<UserName, User> defaultUsers) {
+        this.defaultUsers = defaultUsers;
     }
 
     @Override
     public User retrieveBy(UserName userName) {
-        if (!STANDARD_USERS.containsKey(userName)) {
+        if (defaultUsers == null || !defaultUsers.containsKey(userName)) {
             return null;
         }
 
-        return STANDARD_USERS.get(userName);
-    }
-
-    private void initStandardUsers() {
-        STANDARD_USERS.put(new UserName("jactor"), initJactor());
-        STANDARD_USERS.put(new UserName("tip"), initTip());
+        return defaultUsers.get(userName);
     }
 }
--- hjemme/hjemme-facade/src/test/java/nu/hjemme/facade/config/UserFacadeIntegrationTest.java
+++ hjemme/hjemme-facade/src/test/java/nu/hjemme/facade/config/UserFacadeIntegrationTest.java
@@ -1,12 +1,13 @@
-package nu.hjemme.facade.factory;
+package nu.hjemme.facade.config;
 
 import nu.hjemme.client.datatype.UserName;
 import nu.hjemme.client.service.UserFacade;
-import nu.hjemme.facade.SpringCtx;
+import nu.hjemme.facade.factory.config.HjemmeAppContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import javax.annotation.Resource;
 
@@ -15,8 +16,8 @@ import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {SpringCtx.HJEMME_FACADE_TEST_BEANS})
-public class UserFacadeFactoryIntegrationTest {
+@ContextConfiguration(classes = HjemmeAppContext.class, loader = AnnotationConfigContextLoader.class)
+public class UserFacadeIntegrationTest {
 
     @Resource
     UserFacade testUserFacade;
