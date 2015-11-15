2e8d081963a6b4b604a1414d47206e706bb6601d
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2015-11-15 16:08:09 +0100

    #85: ChosenMenuItem to MenuItem, work in progress, formatting

--- hjemme/hjemme-business/src/test/java/nu/hjemme/business/service/MenuFacadeImplTest.java
+++ hjemme/hjemme-business/src/test/java/nu/hjemme/business/service/MenuFacadeImplTest.java
@@ -5,6 +5,7 @@ import nu.hjemme.client.datatype.MenuItemTarget;
 import nu.hjemme.client.datatype.MenuTarget;
 import nu.hjemme.client.datatype.Name;
 import nu.hjemme.client.domain.menu.Menu;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -21,26 +22,26 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class MenuFacadeImplTest {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    @Rule public ExpectedException expectedException = ExpectedException.none();
+    @Mock static private MenuDomain menuDomainMock;
 
-    @Mock
-    static private MenuDomain menuDomainMock;
+    private MenuFacadeImpl testMenuFacadeImpl;
 
-    @Test
-    public void willThrowExceptionIfProvidedMenusAreNull() {
+    @Before public void initMenuFacadeToTest() {
+        testMenuFacadeImpl = new MenuFacadeImpl(singletonList(menuDomainMock));
+    }
+
+    @Test public void willThrowExceptionIfProvidedMenusAreNull() {
         expectedException.expect(IllegalArgumentException.class);
         new MenuFacadeImpl(null);
     }
 
-    @Test
-    public void willThrowExceptionIfProvidedMenusAreEmpty() {
+    @Test public void willThrowExceptionIfProvidedMenusAreEmpty() {
         expectedException.expect(IllegalArgumentException.class);
         new MenuFacadeImpl(Collections.<Menu>emptyList());
     }
 
-    @Test
-    public void willFailWhenMenuIsUnknown() {
+    @Test public void willFailWhenMenuIsUnknown() {
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("unknown configuration");
         expectedException.expectMessage("known.menu");
@@ -51,20 +52,17 @@ public class MenuFacadeImplTest {
 
         when(menuDomainMock.getName()).thenReturn(knownMenu);
 
-        MenuFacadeImpl testMenuFacadeImpl = new MenuFacadeImpl(singletonList(menuDomainMock));
         testMenuFacadeImpl.fetchMenuItemBy(
                 new MenuTarget(somewhere, new Name("unknown.menu"))
         );
     }
 
-    @Test
-    public void willFindKnownMenuItems() {
+    @Test public void willFindKnownMenuItems() {
         MenuItemTarget somewhere = new MenuItemTarget("somewhere");
         Name knownMenu = new Name("known.menu");
 
         when(menuDomainMock.getName()).thenReturn(knownMenu);
 
-        MenuFacadeImpl testMenuFacadeImpl = new MenuFacadeImpl(singletonList(menuDomainMock));
         testMenuFacadeImpl.fetchMenuItemBy(new MenuTarget(somewhere, knownMenu));
 
         verify(menuDomainMock).fetchMenuItemsBy(somewhere);
