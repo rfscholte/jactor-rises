d0001d1918cee9e3264f031e8ca14610c8fd5475
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2017-07-23 18:26:19 +0200

    #107: UserDomainBuilderTest is using assertJ

--- hjemme/hjemme-business/src/test/java/nu/hjemme/business/domain/builder/PersonDomainBuilderTest.java
+++ hjemme/hjemme-business/src/test/java/nu/hjemme/business/domain/builder/PersonDomainBuilderTest.java
@@ -1,6 +1,5 @@
 package nu.hjemme.business.domain.builder;
 
-import nu.hjemme.business.domain.PersonDomain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -8,23 +7,21 @@ import org.junit.jupiter.api.Test;
 
 import static nu.hjemme.business.domain.AddressDomain.anAddress;
 import static nu.hjemme.business.domain.PersonDomain.aPerson;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("The Person Domain Builder")
 class PersonDomainBuilderTest {
 
+    @DisplayName("should not build an instance without an address")
     @Test void willNotBuildPersonDomainWithoutAnAddress() {
         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> aPerson().build());
         Assertions.assertThat(illegalArgumentException.getMessage()).isEqualTo(PersonDomainBuilder.AN_ADDRESS_MUST_BE_PRESENT);
     }
 
-    @DisplayName("should build a person when all requered fields are set")
+    @DisplayName("should build an instance when all requered fields are set")
     @Disabled() // TODO: #113 fix when all builders are validation builders
     @Test void willBuildPersonDomainWhenAllRequiredFieldsAreSet() throws Exception {
-        PersonDomain personDomain = aPerson().with(anAddress()).withDescriptionAs("description field will not be validated").build();
-
-        assertThat(personDomain, notNullValue());
+        assertThat(aPerson().with(anAddress()).withDescriptionAs("description field for coverage").build()).isNotNull();
     }
 }
