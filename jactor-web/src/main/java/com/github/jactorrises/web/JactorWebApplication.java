b50f6cda6fbe0a5bc32aa86e3f7bd9a46bfb4e2e
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2017-09-01 19:33:01 +0200

    #126: prepare jactor-web to spring-boot, file changes

---
+++ jactor-web/src/main/java/com/github/jactorrises/web/JactorWebApplication.java
@@ -0,0 +1,4 @@
+package com.github.jactorrises.web;
+
+public class JactorWebApplication {
+}
--- jactor-web/src/test/java/com/github/jactorrises/web/JactorWebApplicationIntegrationTest.java
+++ jactor-web/src/test/java/com/github/jactorrises/web/JactorWebApplicationIntegrationTest.java
@@ -1,25 +1,20 @@
-package com.github.jactorrises.web.config;
+package com.github.jactorrises.web;
 
 import com.github.jactorrises.web.controller.AboutController;
 import com.github.jactorrises.web.controller.HomeController;
 import com.github.jactorrises.web.controller.UserController;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(loader = AnnotationConfigWebContextLoader.class, classes = JactorWebApp.class)
-@Ignore("cannot run in ide on mac after upgrade to junit 5, ignored until resolved")
-public class JactorWebAppIntegrationTest {
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {JactorWebApplication.class})
+public class JactorWebApplicationIntegrationTest {
 
     @Resource private HomeController homeController;
 
