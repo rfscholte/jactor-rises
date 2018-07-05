45e26d2dc8db40fdcec4b4e3a131b24f3be1f38b
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2018-07-05 14:01:37 +0200

    #207: jactor-commons is know root of jactor-client and jactor-test, and modules is build using build module jactor-rises and jactor-standalones
    - also simplified build.sh to use these build modules...

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 export MAVEN_OPTS=-Xmx1024m
 
-cd jactor-commons
 mvn clean install
 STATUS=$?
 
@@ -10,23 +9,7 @@ if [ $STATUS -gt 0 ]; then
   exit ${STATUS}
 fi
 
-cd ../jactor-standalones/jactor-persistence-orm
-mvn clean install
-STATUS=$?
-
-if [ $STATUS -gt 0 ]; then
-  exit ${STATUS}
-fi
-
-cd ../../jactor-modules
-mvn clean install
-STATUS=$?
-
-if [ $STATUS -gt 0 ]; then
-  exit ${STATUS}
-fi
-
-cd ../jactor-standalones/jactor-web/
+cd jactor-standalones
 mvn clean install
 STATUS=$?
 
