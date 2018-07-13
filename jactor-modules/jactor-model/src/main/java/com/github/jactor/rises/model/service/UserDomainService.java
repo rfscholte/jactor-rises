61801bbe69dd1164f511b08d051b471203267c95
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2018-07-13 13:53:52 +0200

    issue #132: java 9 modules on jactor.rises

--- jactor-modules/jactor-model/src/main/java/com/github/jactor/rises/model/service/UserDomainService.java
+++ jactor-modules/jactor-model/src/main/java/com/github/jactor/rises/model/service/UserDomainService.java
@@ -4,12 +4,10 @@ import com.github.jactor.rises.client.datatype.Username;
 import com.github.jactor.rises.client.domain.User;
 import com.github.jactor.rises.io.rest.UserRestService;
 import com.github.jactor.rises.model.domain.user.UserDomain;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class UserDomainService {
     private final UserRestService userRestService;
 
@@ -21,11 +19,13 @@ public class UserDomainService {
         return new UserDomain(userRestService.saveOrUpdate(userDomain.getDto()));
     }
 
-    @SuppressWarnings("unchecked") public <T extends User> Optional<T> ferch(Long id) {
+    @SuppressWarnings("unchecked")
+    public <T extends User> Optional<T> ferch(Long id) {
         return userRestService.fetch(id).map(UserDomain::new).map(userDomain -> (T) userDomain);
     }
 
-    @SuppressWarnings("unchecked") public <T extends User> Optional<T> find(Username username) {
+    @SuppressWarnings("unchecked")
+    public <T extends User> Optional<T> find(Username username) {
         return userRestService.find(username).map(UserDomain::new).map(userDomain -> (T) userDomain);
     }
 
