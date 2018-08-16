a48a041bb08b13007c0783195e7be3003861bc5d
rfscholte <rfscholte@apache.org>
2018-08-16 17:53:51 +0200

    Add module descriptor, make module open

---
+++ jactor-applications/jactor-facade/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+open module jactor.rises.facade {
+    requires jactor.rises.commons;
+    requires jactor.rises.model;
+
+    requires slf4j.api;
+
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.web;
+}
